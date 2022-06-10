f14edf4b98f7783293fd36591cb382e2ff474ee3
Pritam Dash <pritamdash2015@outlook.com>
2022-06-10 21:51:21 +0530

    UI of collection View done and images added in image set

---
+++ CollectionViewApp/Offer.swift
@@ -0,0 +1,12 @@
+//
+//  Offer.swift
+//  CollectionViewApp
+//
+//  Created by Pritam Dash on 10/06/22.
+//
+
+import Foundation
+struct Offer {
+    var image : String
+    var details : String
+}
--- CollectionViewApp/ViewController.swift
+++ CollectionViewApp/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var offersCollectionView: UICollectionView!
+    @IBOutlet weak var offersDetailsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
